1d495218b39fa2f42b9e0652d0b4dd7cd6baedb8
SaravananAyyanar <44993310+SaravananAyyanar@users.noreply.github.com>
2018-11-15 09:56:56 +0530

    how-to-change-the-Enter-key-behavior-in-winforms-datagrid

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.WinForms.Controls;
+using Syncfusion.WinForms.DataGrid.Styles;
+using System.Windows.Forms;
+using Syncfusion.WinForms.DataGrid.Enums;
+using Syncfusion.WinForms.DataGrid;
+
+namespace GettingStarted
+{   
+    public partial class Form1 : Form
+    {
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            var data = new OrderInfoCollection();
+            sfDataGrid.DataSource = data.OrdersListDetails;
+
+            this.sfDataGrid.SelectionController = new CustomSelectionController(sfDataGrid);
+        }
+
+        #endregion 
+    }
+
+    public class CustomSelectionController : Syncfusion.WinForms.DataGrid.Interactivity.RowSelectionController
+    {
+        SfDataGrid DataGrid;
+
+        public CustomSelectionController(SfDataGrid sfDataGrid)
+            : base(sfDataGrid)
+        {
+            this.DataGrid = sfDataGrid;
+        }
+
+        protected override void HandleKeyOperations(KeyEventArgs args)
+        {
+            if (args.KeyCode == Keys.Enter)
+            {
+                KeyEventArgs arguments = new KeyEventArgs(Keys.Tab);
+                base.HandleKeyOperations(arguments);
+                return;
+            }
+
+            base.HandleKeyOperations(args);
+        }
+
+    } 
+}
